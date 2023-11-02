de5d97d6fe0f84d69519d8a01791c7a825cf410e
HasanTorman <torman.hassan@gmail.com>
2023-11-02 12:17:26 +0300

    feat: create & update functions

--- src/App.js
+++ src/App.js
@@ -15,27 +15,58 @@ function App() {
     localStorage.setItem('notes', JSON.stringify(notes))
   }, [notes])
 
-  console.log(currentId, 'current id')
-  function addNote(e) {
+  function addNote() {
     const newNote = {
       id: nanoid(),
       body: "# Type your markdown note's title here",
     }
     setNotes((prevNotes) => [newNote, ...prevNotes])
+    setCurrentId(newNote.id)
   }
+
+  function updateNote(text) {
+    setNotes((oldNotes) => {
+      const newArray = []
+      for (let index = 0; index < oldNotes.length; index++) {
+        const element = oldNotes[index]
+        if (element.id === currentId) {
+          newArray.unshift({
+            ...element,
+            body: text,
+          })
+        } else {
+          newArray.push(element)
+        }
+      }
+
+      return newArray
+    })
+  }
+
+  function deleteNote(event, noteId) {
+    setNotes((oldNotes) => oldNotes.filter((note) => noteId !== note.id))
+  }
+
+  const currentNote =
+    notes.find((note) => {
+      return note.id === currentId
+    }) || notes[0]
 
   return (
     <div className="container">
       {notes.length > 0 ? (
-        <>
+        <Split sizes={[30, 70]} direction="horizontal" className="split">
           <Sidebar
             addNote={addNote}
             notes={notes}
             setCurrentId={setCurrentId}
+            currentId={currentId}
+            deleteNote={deleteNote}
           />
-        </>
+          <Editor currentNote={currentNote} updateNote={updateNote} />
+        </Split>
       ) : (
-        <button onClick={(event) => addNote(event)}>Create New Note</button>
+        <button onClick={addNote}>Create New Note</button>
       )}
     </div>
   )
